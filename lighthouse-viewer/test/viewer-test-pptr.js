02a48006c47d151787d0c8e00d0a7a5d8b052acc
Connor Clark <cjamcl@google.com>
2019-08-09 15:15:33 -0500

    viewer: psi api

--- lighthouse-viewer/test/viewer-test-pptr.js
+++ lighthouse-viewer/test/viewer-test-pptr.js
@@ -8,6 +8,7 @@
 /* eslint-env jest */
 
 const path = require('path');
+const fs = require('fs');
 const assert = require('assert');
 const puppeteer = require('../../node_modules/puppeteer/index.js');
 
@@ -21,13 +22,18 @@ const lighthouseCategories = Object.keys(config.categories);
 const getAuditsOfCategory = category => config.categories[category].auditRefs;
 
 // TODO: should be combined in some way with clients/test/extension/extension-test.js
-describe('Lighthouse Viewer', function() {
+describe('Lighthouse Viewer', () => {
   // eslint-disable-next-line no-console
   console.log('\n✨ Be sure to have recently run this: yarn build-viewer');
 
   let browser;
   let viewerPage;
   const pageErrors = [];
+
+  const selectors = {
+    audits: '.lh-audit, .lh-metric',
+    titles: '.lh-audit__title, .lh-metric__title',
+  };
 
   function getAuditElementsIds({category, selector}) {
     return viewerPage.evaluate(
@@ -59,16 +65,12 @@ describe('Lighthouse Viewer', function() {
     });
     viewerPage = await browser.newPage();
     viewerPage.on('pageerror', pageError => pageErrors.push(pageError));
-    await viewerPage.goto(viewerUrl, {waitUntil: 'networkidle2', timeout: 30000});
-    const fileInput = await viewerPage.$('#hidden-file-input');
-    await fileInput.uploadFile(sampleLhr);
-    await viewerPage.waitForSelector('.lh-container', {timeout: 30000});
   });
 
   afterAll(async function() {
     // Log any page load errors encountered in case before() failed.
     // eslint-disable-next-line no-console
-    console.error(pageErrors);
+    if (pageErrors.length > 0) console.error(pageErrors);
 
     await Promise.all([
       new Promise(resolve => server.close(resolve)),
@@ -76,63 +78,160 @@ describe('Lighthouse Viewer', function() {
     ]);
   });
 
+  describe('Upload', () => {
+    beforeAll(async function() {
+      await viewerPage.goto(viewerUrl, {waitUntil: 'networkidle2', timeout: 30000});
+      const fileInput = await viewerPage.$('#hidden-file-input');
+      await fileInput.uploadFile(sampleLhr);
+      await viewerPage.waitForSelector('.lh-container', {timeout: 30000});
+    });
 
-  const selectors = {
-    audits: '.lh-audit, .lh-metric',
-    titles: '.lh-audit__title, .lh-metric__title',
-  };
+    it('should load with no errors', async () => {
+      assert.deepStrictEqual(pageErrors, []);
+    });
 
-  it('should load with no errors', async () => {
-    assert.deepStrictEqual(pageErrors, []);
-  });
-
-  it('should contain all categories', async () => {
-    const categories = await getCategoryElementsIds();
-    assert.deepStrictEqual(
-      categories.sort(),
-      lighthouseCategories.sort(),
-      `all categories not found`
-    );
-  });
-
-  it('should contain audits of all categories', async () => {
-    for (const category of lighthouseCategories) {
-      let expected = getAuditsOfCategory(category);
-      if (category === 'performance') {
-        expected = getAuditsOfCategory(category).filter(a => !!a.group);
-      }
-      expected = expected.map(audit => audit.id);
-      const elementIds = await getAuditElementsIds({category, selector: selectors.audits});
-
+    it('should contain all categories', async () => {
+      const categories = await getCategoryElementsIds();
       assert.deepStrictEqual(
-        elementIds.sort(),
-        expected.sort(),
-        `${category} does not have the identical audits`
+        categories.sort(),
+        lighthouseCategories.sort(),
+        `all categories not found`
       );
+    });
+
+    it('should contain audits of all categories', async () => {
+      for (const category of lighthouseCategories) {
+        let expected = getAuditsOfCategory(category);
+        if (category === 'performance') {
+          expected = getAuditsOfCategory(category).filter(a => !!a.group);
+        }
+        expected = expected.map(audit => audit.id);
+        const elementIds = await getAuditElementsIds({category, selector: selectors.audits});
+
+        assert.deepStrictEqual(
+          elementIds.sort(),
+          expected.sort(),
+          `${category} does not have the identical audits`
+        );
+      }
+    });
+
+    it('should contain a filmstrip', async () => {
+      const filmstrip = await viewerPage.$('.lh-filmstrip');
+
+      assert.ok(!!filmstrip, `filmstrip is not available`);
+    });
+
+    it('should not have any unexpected audit errors', async () => {
+      function getErrors(elems, selectors) {
+        return elems.map(el => {
+          const audit = el.closest(selectors.audits);
+          const auditTitle = audit && audit.querySelector(selectors.titles);
+          return {
+            explanation: el.textContent,
+            title: auditTitle ? auditTitle.textContent : 'Audit title unvailable',
+          };
+        });
+      }
+
+      const errorSelectors = '.lh-audit-explanation, .tooltip--error';
+      const auditErrors = await viewerPage.$$eval(errorSelectors, getErrors, selectors);
+      const errors = auditErrors.filter(item => item.explanation.includes('Audit error:'));
+      assert.deepStrictEqual(errors, [], 'Audit errors found within the report');
+    });
+  });
+
+  describe('PSI', () => {
+    let onApiRequestInterception;
+    let apiRequestInterceptionResolve;
+
+    function onRequest(interceptedRequest) {
+      if (interceptedRequest.url().includes('https://www.googleapis.com')) {
+        apiRequestInterceptionResolve(interceptedRequest);
+      } else {
+        interceptedRequest.continue();
+      }
     }
-  });
 
-  it('should contain a filmstrip', async () => {
-    const filmstrip = await viewerPage.$('.lh-filmstrip');
+    beforeAll(async () => {
+      await viewerPage.setRequestInterception(true);
+      viewerPage.on('request', onRequest);
+    });
 
-    assert.ok(!!filmstrip, `filmstrip is not available`);
-  });
+    afterAll(async () => {
+      viewerPage.off('request', onRequest);
+      await viewerPage.setRequestInterception(false);
+    });
 
-  it('should not have any unexpected audit errors', async () => {
-    function getErrors(elems, selectors) {
-      return elems.map(el => {
-        const audit = el.closest(selectors.audits);
-        const auditTitle = audit && audit.querySelector(selectors.titles);
-        return {
-          explanation: el.textContent,
-          title: auditTitle ? auditTitle.textContent : 'Audit title unvailable',
-        };
+    beforeEach(() => {
+      onApiRequestInterception = new Promise(resolve => apiRequestInterceptionResolve = resolve);
+    });
+
+    it('should call out to PSI with all categories by default', async () => {
+      const url = `${viewerUrl}?url=https://www.example.com`;
+      await viewerPage.goto(url);
+
+      // Intercept and respond with sample lhr.
+      const interceptedRequest = await onApiRequestInterception;
+      const interceptedUrl = new URL(interceptedRequest.url());
+      expect(interceptedUrl.origin + interceptedUrl.pathname).toEqual('https://www.googleapis.com/pagespeedonline/v5/runPagespeed');
+      expect({
+        url: interceptedUrl.searchParams.get('url'),
+        category: interceptedUrl.searchParams.getAll('category'),
+      }).toEqual({
+        url: 'https://www.example.com',
+        category: [
+          'performance',
+          'accessibility',
+          'seo',
+          'best-practices',
+          'pwa',
+        ],
       });
-    }
+      const psiResponse = {
+        lighthouseResult: JSON.parse(fs.readFileSync(sampleLhr, 'utf-8')),
+      };
+      await interceptedRequest.respond({
+        status: 200,
+        contentType: 'application/json',
+        body: JSON.stringify(psiResponse),
+      });
 
-    const errorSelectors = '.lh-audit-explanation, .tooltip--error';
-    const auditErrors = await viewerPage.$$eval(errorSelectors, getErrors, selectors);
-    const errors = auditErrors.filter(item => item.explanation.includes('Audit error:'));
-    assert.deepStrictEqual(errors, [], 'Audit errors found within the report');
+      // Wait for report to render.
+      await viewerPage.waitForSelector('.lh-columns');
+
+      // No errors.
+      assert.deepStrictEqual(pageErrors, []);
+
+      // All categories.
+      const categories = await getCategoryElementsIds();
+      assert.deepStrictEqual(
+        categories.sort(),
+        lighthouseCategories.sort(),
+        `all categories not found`
+      );
+    });
+
+    it('should call out to PSI with specified categoeries', async () => {
+      const url = `${viewerUrl}?url=https://www.example.com&categories=seo,pwa`;
+      await viewerPage.goto(url);
+
+      const interceptedRequest = await onApiRequestInterception;
+      const interceptedUrl = new URL(interceptedRequest.url());
+      expect(interceptedUrl.origin + interceptedUrl.pathname).toEqual('https://www.googleapis.com/pagespeedonline/v5/runPagespeed');
+      expect({
+        url: interceptedUrl.searchParams.get('url'),
+        category: interceptedUrl.searchParams.getAll('category'),
+      }).toEqual({
+        url: 'https://www.example.com',
+        category: [
+          'seo',
+          'pwa',
+        ],
+      });
+
+      // Just wanted to check the params are correct, let's abort. Previous test does more of an end-to-end test.
+      interceptedRequest.abort();
+    });
   });
 });
